7896d870af4810e6e93700bb466c9156f3f1b4ed
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2021-01-18 01:17:33 +0100

    search functionality

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="hello">
+    <form @submit.prevent="searchFunction">
+      <input v-model="search" type="text" name="" id="">
+    </form>
+    <h3>Results for <span>"{{search}}"</span></h3>
     <div class="f" v-for="(item, index) in data" :key="index">
       {{item.title}}<br>
-      <a target="_blank" :href="'https://en.wikipedia.org/?curid=' + item.pageid">{{'https://en.wikipedia.org/?curid=' + item.pageid}}</a><br>
+      <a target="_blank" :href="`https://en.wikipedia.org/?curid=${item.pageid}`">{{'https://en.wikipedia.org/?curid=' + item.pageid}}</a><br>
       {{item.snippet}}
       <hr />
     </div>
@@ -16,16 +20,17 @@ export default {
   setup() {
 
     const data = ref([])
-    const url = ref('')
+    const search = ref('')
+    function searchFunction(){
+      axios.get('https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=' + search.value)
+      .then(res => {
+        console.log(res.data.query.search)
+        data.value = res.data.query.search
+      })
+    }
 
-    axios.get('https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=javascript')
-    .then(res => {
-      console.log(res.data.query.search)
-      data.value = res.data.query.search
-    })
 
-
-    return{ data, url }
+    return{ data, search, searchFunction }
   }
 }
 </script>
